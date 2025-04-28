da40c3b59a664d7b9314075e7bc5f3b088885a8f
Owen Clarke <oc8825@princeton.edu>
2025-04-28 13:53:30 -0400

    using fputc instead of putc

--- createdataB.c
+++ createdataB.c
@@ -11,11 +11,11 @@ int main(void)
     fprintf(psFile, "Ben Zhou and Owen Clarke");
 
     /* Terminating null byte */
-    putc(psFile, '\0');
+    fputc(psFile, '\0');
 
     /* 23 more null bytes to fill up 48 bytes of buffer */
     for (i = 0; i < 23; i++)
-        putc(psFile, '\0');
+        fputc(psFile, '\0');
 
     /* Return address for changing grade to B */
     fprintf(psFile, "0x400894");
