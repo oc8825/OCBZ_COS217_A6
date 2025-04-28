3e7c0c4f998430c94ab9aaf98de8347ca02fd7bb
Owen Clarke <oc8825@princeton.edu>
2025-04-28 16:30:27 -0400

    off by one other way?

--- createdataB.c
+++ createdataB.c
@@ -14,7 +14,7 @@ int main(void)
     putc('\0', psFile);
 
     /* 23 more null bytes to fill up 48 bytes of buffer */
-    for (i = 0; i < 24; i++)
+    for (i = 0; i < 22; i++)
         putc('\0', psFile);
 
     /* Return address for changing grade to B */
