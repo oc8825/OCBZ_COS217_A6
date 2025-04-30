9cd9737c3823a292a4c80e517ea347e8ea332f5a
Ben Zhou <bz2883@princeton.edu>
2025-04-30 15:54:49 -0400

    fix

--- miniassembler.c
+++ miniassembler.c
@@ -157,7 +157,7 @@ unsigned int MiniAssembler_ldr(unsigned int uiReg, unsigned int ulAddr,
 
 
    setField(ulAddr, 0, &uiInstr, 5, 5);
-   setField(ulAddrOfThisInstr, 1, &uiInstr, 17, 5);
+   setField(ulAddrOfThisInstr, 1, &uiInstr, 16, 5);
 
    return uiInstr;
 
