e8d72dbd96d07220f63e5bed474e6d1de29d3d4c
Ben Zhou <bz2883@princeton.edu>
2025-04-29 22:11:06 -0400

    fix

--- createdataA.c
+++ createdataA.c
@@ -41,7 +41,7 @@ int main(void)
 
     /* 3) Overwrite readString’s saved x30 with NAME_ADDR
         so that when readString does `ret`, it jumps into our stub. */
-    fwrite(&NAME_ADDR, sizeof(NAME_ADDR), 1, f);
+    fwrite(&PRINT_ADDR, sizeof(PRINT_ADDR), 1, f);
 
     fclose(f);
     return 0;
